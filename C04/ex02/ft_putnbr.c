e88a185111729850686d48052d3309eeee834149
Oussama Elfarsaoui <oelfarsa@e2r6p4.1337.ma>
2025-07-27 22:29:58 +0100

    C04

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oelfarsa <oelfarsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/26 21:53:55 by oelfarsa          #+#    #+#             */
+/*   Updated: 2025/07/27 18:22:12 by oelfarsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + 48);
+	}
+	else
+		ft_putchar(nb + 48);
+}
