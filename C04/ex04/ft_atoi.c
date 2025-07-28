49d56a7a10b0158d95e147013bbc9e93fd4df1bd
Fornax <fornax@frx.team>
2025-07-28 01:28:49 +0100

    commit 1

--- C04/ex04/ft_atoi.c
+++ C04/ex04/ft_atoi.c
@@ -10,7 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	space(char  c)
+{
+
+}
+
 int	ft_atoi(char *str)
 {
-	
+	int i;
+	int	result;
+	int sign;
+
+    i = 0;
+	result = 0;
+	sign = 1;
+	while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if(str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + str[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf("%d\n", ft_atoi("+222"));
+	printf("%d", atoi("-222"));
 }
